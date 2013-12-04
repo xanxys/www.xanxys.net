e25be3741e5250218d821060bb9e7194bb4a0b24
xanxys <xanxys@gmail.com>
2013-12-05 00:20:37 +0900

    Update bonsai to r3.

---
+++ http/bonsai/script/isolated_chunk.js
@@ -0,0 +1,44 @@
+importScripts('./underscore.js');
+importScripts('./three.js');
+importScripts('./chunk.js');
+
+var ChunkServer = function() {
+	var scene = new THREE.Scene();
+	this.chunk = new Chunk(scene);
+
+	this.current_plant = this.chunk.add_plant(
+		new THREE.Vector3(0, 0, 0),
+		Math.pow(20e-3, 3) * 100 // allow 2cm cube for 100T
+	);
+
+	var _this = this;
+	self.addEventListener('message', function(ev) {
+		if(ev.data.type === 'step') {
+			var sim_stat = _this.chunk.step();
+			self.postMessage({
+				type: 'stat-sim',
+				data: sim_stat
+			});
+		} else if(ev.data.type === 'serialize') {
+			self.postMessage({
+				type: 'serialize',
+				data: _this.chunk.serialize()
+			});
+		} else if(ev.data.type === 'stat') {
+			self.postMessage({
+				type: 'stat-chunk',
+				data: _this.chunk.get_stat()
+			});
+		} else if(ev.data.type === 'stat-plant') {
+			self.postMessage({
+				type: 'stat-plant',
+				data: {
+					id: ev.data.data.id,
+					stat: _this.chunk.get_plant_stat(ev.data.data.id)
+				}
+			});
+		}
+	});
+};
+
+new ChunkServer();
